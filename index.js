99144bf894713aa44f8bdba9709ea301e80b2b1e
Kernela <112568330+KernelaKlees@users.noreply.github.com>
2026-05-29 20:44:16 +0100

    Add emojify functions to convert text to emojis

---
+++ index.js
@@ -0,0 +1,27 @@
+const hackedEmojis = {
+    ":angry:": "🎁",
+    ":cry:": "😄",
+    ":thumbsdown:": "👏",
+    ":puke:": "🍫"
+};
+
+function emojifyWord(word) {
+    if (word.startsWith(":") && word.endsWith(":")) {
+        if (hackedEmojis[word]) {
+            return hackedEmojis[word];
+        }
+    }
+    return word;
+}
+
+function emojifyPhrase(phrase) {
+    const words = phrase.split(" " );
+    const emojifiedWords = words.map(emojifyWord);
+    return emojifiedWords.join(" ");
+}
+
+const inputPhrase = "Just read your article :thumbsdown: and :puke:!";
+const outputPhrase = emojifyPhrase(inputPhrase);
+
+const resultElement = document.getElementById('result');
+resultElement.innerText = `Original: ${inputPhrase}\n\nEmojified: ${outputPhrase}`;
